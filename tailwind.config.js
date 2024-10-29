98217ad8679913f93837563ffef830bf1d3e9a66
Maurice <mauricemiema90@gmail.com>
2024-10-30 00:14:33 +0200

    configuration de versel et ajout du dossier public pour le deploiement de site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./index.js",],
+  content: [
+    "./public/**/*.html",
+    "./public/**/*.js",
+  ],
   theme: {
     extend: {
       fontFamily:{
